e301bd7e53108927c8d8db641f02d62922c07446
karlreg1 <166554837+karlreg1@users.noreply.github.com>
2024-11-06 16:54:47 +0100

    added stylesheed expanded html

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer((res, req)=>{
+const server = http.createServer((req, res)=>{
     fs.readFile('index.html',function(err, data){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
         });
+    console.log("log");
 });
 server.listen(8080);
