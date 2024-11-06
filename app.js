a4cccaad7b0eb5053aa2874fc516dac8d4caaeeb
karlreg1 <166554837+karlreg1@users.noreply.github.com>
2024-11-06 18:57:45 +0100

    added the tictactoe game

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res)=>{
-    fs.readFile('index.html',function(err, data){
+    fs.readFile('tiktik.html',function(err, data){
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
