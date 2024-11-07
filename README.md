dcc7d4cfa1fd23d7cd70543ff27b1a940b420fef
karlreg1 <166554837+karlreg1@users.noreply.github.com>
2024-11-07 17:35:51 +0100

    added the public folder changed up app.js expanded index.html and style.css

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # meisenchat
+
+
+colors used:
+    dark pink: FF58C4
+    medium pink: FFB8E6
+    light pink: FFE0F0
+    white: smokewhite
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const fs = require('fs');
 const express = require('express')
 const app = express();
+const path = require('path');
 
-app.get("/",(req, res) =>{
+app.use(express.static('public'));
+
+
+app.get("/",(req, res, next) =>{
+    res.sendFile(path.join(__dirname + "/index.html"));
     console.log("home");
 });
 
@@ -10,5 +14,8 @@ app.get("/login/",(req, res)=>{
     console.log("login");
 });
 
+function authenticateUser(req, res, next){
+    
+}
 
 app.listen(8080);
