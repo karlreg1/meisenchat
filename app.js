2218f5cc1d2b3ce0a5a0d482ee2d8277ac4faca9
karlreg1 <166554837+karlreg1@users.noreply.github.com>
2024-11-07 16:43:01 +0100

    redid the app.js file added a GET-method for login and home.

--- app.js
+++ app.js
@@ -1,26 +1,14 @@
-const http = require('http');
 const fs = require('fs');
 const express = require('express')
+const app = express();
 
-
-const server = http.createServer((req, res)=>{
-    if (req.url == '/'){
-        console.log(200);
-        fs.readFile('index.html',function(err, data){
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-        });
-    
-    } else {
-        // Handle 404 for other routes
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>404 Not Found</h1>', 'utf-8');
-    }
-    
- });
-
-
-server.listen(8080, ()=>{
-    console.log(`Server running at http://localhost:${PORT}`);
+app.get("/",(req, res) =>{
+    console.log("home");
 });
+
+app.get("/login/",(req, res)=>{
+    console.log("login");
+});
+
+
+app.listen(8080);
