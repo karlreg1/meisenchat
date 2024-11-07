b7c9a7c63c4fc169c0c8ce0da744e46b2ded4712
karlreg1 <166554837+karlreg1@users.noreply.github.com>
2024-11-07 14:53:22 +0100

    added the 404 message chaneged the name back to style.css imported express

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const http = require('http');
 const fs = require('fs');
+const express = require('express')
+
 
 const server = http.createServer((req, res)=>{
-    fs.readFile('index.html',function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
+    if (req.url == '/'){
+        console.log(200);
+        fs.readFile('index.html',function(err, data){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            return res.end();
         });
-    console.log("log");
+    
+    } else {
+        // Handle 404 for other routes
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>', 'utf-8');
+    }
+    
+ });
+
+
+server.listen(8080, ()=>{
+    console.log(`Server running at http://localhost:${PORT}`);
 });
-server.listen(8080);
